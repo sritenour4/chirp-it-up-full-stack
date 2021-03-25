33c47e523f28c4009238a8a755ca11d3d8bcbb3a
Shannon Ritenour <shannonritenour@gmail.com>
2021-03-25 15:40:40 -0500

    updated chirps query

--- src/server/db/chirps.ts
+++ src/server/db/chirps.ts
@@ -1,11 +1,11 @@
 import { Query} from './index';
 
 // const all = async () => Query('SELECT * FROM chirpr.chirps');
-const all = async () => Query('SELECT * FROM chirps');
-const one = async (userid: string) => Query("SELECT * FROM chirps WHERE chirps.id = ?" [userid]);
-const post = async (userid: string, content: string, location: string) => Query('INSERT INTO chirps (userid, content, location) values (?,?,?)', [userid, content, location]);
-const put = async (userid: string, content: string) => Query("UPDATE chirps SET content = ? WHERE userid = ?", [userid, content]);
-const destroy = async (userid: string) => Query("DELETE FROM chirps WHERE userid = ?", [userid]);
+const all = () => Query('SELECT * FROM chirps');
+const one = (userid: string) => Query("SELECT * FROM chirps WHERE chirps.id = ?" [userid]);
+const post = (userid: string, content: string, location: string) => Query('INSERT INTO chirps (userid, content, location) values (?,?,?)', [userid, content, location]);
+const put = (userid: string, content: string) => Query("UPDATE chirps SET content = ? WHERE userid = ?", [userid, content]);
+const destroy = (userid: string) => Query("DELETE FROM chirps WHERE userid = ?", [userid]);
 
 export default {
     all,
